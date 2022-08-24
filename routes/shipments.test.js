47ea624600b7730091f55cd730c5f204784d146e
Brenda Liu <brenda.liu24@gmail.com>
2022-08-23 19:38:26 -0500

    shipItAPI test not working

--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -5,7 +5,20 @@ const app = require("../app");
 
 
 describe("POST /", function () {
-  test("valid", async function () {
+  test("valid request body", async function () {
+    const resp = await request(app).post("/shipments").send({
+      productId: 1000,
+      name: "Test Tester",
+      addr: "100 Test St",
+      zipcode: "12345-6789",
+    });
+
+    expect(resp.body).toEqual({ shipped: expect.any(Number) });
+  });
+});
+
+describe("POST /", function () {
+  test("invalid request body zipcode key", async function () {
     const resp = await request(app).post("/shipments").send({
       productId: 1000,
       name: "Test Tester",
@@ -13,6 +26,15 @@ describe("POST /", function () {
       zip: "12345-6789",
     });
 
-    expect(resp.body).toEqual({ shipped: expect.any(Number) });
+    expect(resp.body).toEqual({
+      "error": {
+        "message": [
+          "instance is not allowed to have the additional property \"zip\"",
+          "instance requires property \"zipcode\""
+        ],
+        "status": 400
+      }
+    });
   });
 });
+
--- shipItApi.test.js
+++ shipItApi.test.js
@@ -1,17 +1,48 @@
 "use strict";
+const AxiosMockAdapter = require('axios-mock-adapter');
+const axios = require('axios');
+const axiosMock = new AxiosMockAdapter(axios);
 
 const {
   shipProduct,
+  SHIPIT_SHIP_URL
 } = require("./shipItApi");
 
 
+
 test("shipProduct", async function () {
-  const shipId = await shipProduct({
+  
+  // const mockRes = {receipt:{ 
+  //     itemId:1000, 
+  //     name:'Test Tester', 
+  //     addr:"100 Test St", 
+  //     zip:"12345-6789", 
+  //     shipId:1111 
+  //   }};
+  
+
+
+  
+  axiosMock.onPost(SHIPIT_SHIP_URL).reply(200, {receipt:{ 
+      itemId:1000, 
+      name:"Test Tester", 
+      addr:"100 Test St", 
+      zip:"12345-6789", 
+      shipId:1111 
+    }});
+  
+
+  
+  let res = await shipProduct({
     productId: 1000,
     name: "Test Tester",
     addr: "100 Test St",
-    zip: "12345-6789",
-  });
+    zip: "12345-6789"});
+  
 
-  expect(shipId).toEqual(expect.any(Number));
+  
+  expect(res).toEqual(1111);
 });
+
+
+
